db6aeab5bc3894cdd1d7f29b5332b45603c973f7
Educorreia932 <skelozard1@gmail.com>
2021-05-07 10:47:19 +0100

    Created a class to represent a Chord's node

---
+++ src/chord/ChordNode.java
@@ -0,0 +1,42 @@
+package chord;
+
+import utils.Pair;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class ChordNode {
+    private int id;
+    private List<Pair<Integer, InetSocketAddress>> fingers;
+
+    public ChordNode() {
+
+    }
+
+    public void join() {
+
+    }
+
+    public void stabilize() {
+
+    }
+
+    public void notify(int id) {
+    }
+
+    public void fix_fingers() {
+
+    }
+
+    public void closestPrecidingNode(int id) {
+
+    }
+
+    public void findSuccessor(int id) {
+
+    }
+
+    public void checkPredecessor() {
+
+    }
+}
