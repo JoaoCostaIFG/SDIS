537dd68c6b49859b8490539d71435511f1aa609a
Joao Martins <up201806436@fe.up.pt>
2021-05-14 12:08:17 +0100

    Add: Cypher suites

--- src/sender/SockThread.java
+++ src/sender/SockThread.java
@@ -15,6 +15,16 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SockThread implements Runnable {
     private static final int MAX_CONNS = 5;
+    private static final String[] CYPHER_SUITES = new String[]{
+            "SSL_RSA_WITH_RC4_128_MD5",
+            "SSL_RSA_WITH_RC4_128_SHA",
+            "SSL_RSA_WITH_NULL_MD5",
+            "TLS_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_RSA_WITH_AES_128_CBC_SHA",
+            "TLS_DHE_DSS_WITH_AES_128_CBC_SHA",
+            "TLS_DH_anon_WITH_AES_128_CBC_SHA"
+    };
+
     private final AtomicBoolean running = new AtomicBoolean(false);
     private final ExecutorService threadPool =
             Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
@@ -31,6 +41,7 @@ public class SockThread implements Runnable {
 
         this.serverSocket =
                 (SSLServerSocket) SSLServerSocketFactory.getDefault().createServerSocket(port, MAX_CONNS, ip);
+        this.serverSocket.setEnabledCipherSuites(CYPHER_SUITES);
     }
 
     public String getName() {
@@ -104,6 +115,7 @@ public class SockThread implements Runnable {
         System.out.println("Sent: " + message);
         try {
             SSLSocket socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(address, port);
+            socket.setEnabledCipherSuites(CYPHER_SUITES);
             OutputStream socketWriter = socket.getOutputStream();
             socketWriter.write(packetContent);
             socket.close();
