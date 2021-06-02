9285e8465f5332e50d4a4bfaa3d667068057fb13
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-06-02 17:39:48 +0100

    THREAD POOLS NO READ.

--- src/sender/SSLEngineData.java
+++ src/sender/SSLEngineData.java
@@ -3,12 +3,15 @@ package sender;
 import javax.net.ssl.SSLEngine;
 import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SSLEngineData {
     public boolean isServer;
     public SSLEngine engine;
     public ByteBuffer myAppData, myNetData, peerAppData, peerNetData;
     public ByteArrayOutputStream content;
+    public ExecutorService thread;
 
     public SSLEngineData(SSLEngine engine, ByteBuffer myAppData, ByteBuffer myNetData,
                          ByteBuffer peerAppData, ByteBuffer peerNetData, boolean isServer) {
@@ -19,5 +22,8 @@ public class SSLEngineData {
         this.peerNetData = peerNetData;
         this.content = new ByteArrayOutputStream();
         this.isServer = isServer;
+
+        if (isServer) this.thread = Executors.newSingleThreadExecutor();
+        else this.thread = null;
     }
 }
